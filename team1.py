9958fb90a7775e449d0419afd6f4c46489ba5ae0
yousefghanem <yaghanem2005@gmail.com>
2020-02-19 20:58:54 +0000

    update name

--- team1.py
+++ team1.py
@@ -6,25 +6,20 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
+team_name = 'S & Y' # Only 10 chars displayed.
 strategy_name = 'The name the team gives to this strategy'
 strategy_description = 'How does this strategy decide?'
     
 def move(my_history, their_history, my_score, their_score):
-    ''' Arguments accepted: my_history, their_history are strings.
-    my_score, their_score are ints.
-    
-    Make my move.
-    Returns 'c' or 'b'. 
-    '''
 
-    # my_history: a string with one letter (c or b) per round that has been played with this opponent.
-    # their_history: a string of the same length as history, possibly empty. 
-    # The first round between these two players is my_history[0] and their_history[0].
-    # The most recent round is my_history[-1] and their_history[-1].
-    
-    # Analyze my_history and their_history and/or my_score and their_score.
-    # Decide whether to return 'c' or 'b'.
-    
+  ''' This strategy works based on the other person's score. If my score is less then theirs, then it will betray. If my score is greater than theirs, then it will collude. '''
+  if my_score <= their_score:
+    return "b"
+  elif my_score >= their_score:
     return 'c'
+  else:
+    return 'b'
+  if my_score <= -5000 and their_score >= my_score:
+    return 'b'
+
 
